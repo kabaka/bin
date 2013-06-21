825cb66a5222780035adf9a8e1276a0640e99a45
Kyle Jonhson <kyle@vacantminded.com>
2013-06-21 00:06:44 +0000

    backups: new ftp backup utility

---
+++ backup-utils/ftp/perform-ftp-backups.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+Dir.chdir __dir__
+
+password_file = '.passwd'
+
+unless File.exists? password_file
+  abort 'no password file'
+end
+
+IO.read(password_file).each_line.each_with_index do |line, line_num|
+  host, user_name, password = line.chomp.split(/\s/, 3)
+
+  if password.nil? or password.empty?
+    $stderr.puts "skipping line #{line_num} due to parse error"
+  end
+
+  system "wget -mNc \"ftp://#{user_name}:#{password}@#{host}\" -P ~/backups/ftp/"
+end
+
