305b80a308c67a9f517e1c95e22abb9313c618cb
Kyle Jonhson <kyle@vacantminded.com>
2013-11-17 00:58:41 +0000

    wallbase: update to work with new site

--- wallbase.sh
+++ wallbase.sh
@@ -1,29 +1,33 @@
 #!/bin/zsh
 
-if [ $# -eq 2 ]; then
-
-  TEMPFILE=`mktemp`
-  TEMPLIST=`mktemp`
-
-  curl -q "http://wallbase.cc/search/_$1_/0/eqeq/0x0/0/110/60/relevance/wallpapers/$2" | egrep -o "http://wallbase.cc/wallpaper/[0-9]+" >> $TEMPFILE
-  
-  for line in $(< $TEMPFILE); do 
-    echo "Line: $line"
-    curl -q "$line" | tee | egrep -A 3 "bigwall" | egrep -o "http://[^ \"']+" >> $TEMPLIST
-  done
-
-  rm $TEMPFILE
-
-  SORTTEMP=`mktemp`
-
-  sort -u $TEMPLIST > $SORTTEMP
-  
-  rm $TEMPLIST
-
-  cat $SORTTEMP
-
-  aria2c -i$SORTTEMP -x1 -j1 --retry-wait=60 -m10 -d ~/downloads/wallbase/`echo "$1" | sed -r "s/[^A-Za-z]/_/g"`/
-
-  rm $SORTTEMP
+if [ $# -ne 2 ]; then
+  echo "Usage: ${0} start_image query"
+  echo ""
+  echo "Note: query is not escaped or URL encoded"
+  exit 1
 fi
+
+TEMPFILE=`mktemp`
+TEMPLIST=`mktemp`
+
+curl -q "http://wallbase.cc/search/$1?q=$2" | grep -Eo "http://wallbase.cc/wallpaper/[0-9]+" >> $TEMPFILE
+
+for line in $(< $TEMPFILE); do 
+  echo "Line: $line"
+  curl -q "$line" | tee | grep -E 'class="wall stage1' | grep -Eo "http://[^\"]+" >> $TEMPLIST
+done
+
+rm $TEMPFILE
+
+SORTTEMP=`mktemp`
+
+sort -u $TEMPLIST > $SORTTEMP
+
+rm $TEMPLIST
+
+cat $SORTTEMP
+
+aria2c -i$SORTTEMP -x1 -j1 --retry-wait=60 -m10 -d ~/downloads/wallbase/`echo "$1" | sed -r "s/[^A-Za-z]/_/g"`/
+
+rm $SORTTEMP
 
