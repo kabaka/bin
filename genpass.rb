514b909c551f481e9d486ad6c9cda49ea59c20cc
Kyle Johnson <kyle@vacantminded.com>
2015-06-24 01:20:36 +0000

    genpass: accept multiple arguments, improve handling of invalid requests

--- genpass.rb
+++ genpass.rb
@@ -1,13 +1,16 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 def rand_string length = 8
-  o      = [('a'..'z'),('A'..'Z'),('0'..'9')].map{ |i| i.to_a }.flatten;  
-  string = (1..length).map{ o[rand(o.length)] }.join;
+  if length.zero?
+    warn 'ignoring request for empty or non-numeric length'
+    return
+  end
+
+  o      = [('a'..'z'),('A'..'Z'),('0'..'9')].map{ |i| i.to_a }.flatten
+  string = (1..length).map { o[rand(o.length)] }.join
 end
 
-if ARGV.empty?
-  puts rand_string
-else
-  puts rand_string ARGV.first.to_i
-end
+ARGV << 8 if ARGV.empty?
+
+puts ARGV.map { |a| rand_string a.to_i }
 
