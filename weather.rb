59651e2a10936f7a898b82cb5781b8bad16dbdaa
Kyle Johnson <kyle@vacantminded.com>
2015-06-24 01:13:05 +0000

    weather: improve code style, abort on error

--- weather.rb
+++ weather.rb
@@ -8,21 +8,22 @@ require 'em-http-request'
 # overkill to use EM for one request? maybe
 
 EventMachine.run do
-  http = EventMachine::HttpRequest.new('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml').get :query => {'query' => '75254'}
+  http = EventMachine::HttpRequest.new(
+    'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml'
+  ).get query: {'query' => '75254'}
 
   http.errback do
-    puts 'Weather conditions could not be retrieved.'
-    EM.stop
+    abort 'Weather conditions could not be retrieved.'
   end
 
   http.callback do
-    root = (REXML::Document.new(http.response)).root
+    root = REXML::Document.new(http.response).root
 
     weather = root.elements["//weather"].text rescue nil
 
     temperature = root.elements["//temperature_string"].text
-    humidity = root.elements["//relative_humidity"].text
-    wind = root.elements["//wind_string"].text
+    humidity    = root.elements["//relative_humidity"].text
+    wind        = root.elements["//wind_string"].text
 
     puts "Current Weather Conditions: #{weather}, #{temperature}, #{humidity} Humidity, Wind #{wind}"
 
